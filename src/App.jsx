b5ce37ed69bb4a1900074a2ff5e5da5a30692d4b
DW <danwang809@gmail.com>
2024-02-24 18:44:56 -0800

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import SelectedVideo from './components/SelectedVideo/SelectedVideo';
 import Comments from './components/Comments/Comments';
 import NextVideos from './components/NextVideos/NextVideos';
 import videoData from "../src/data/video-details.json";
+import { useState } from 'react';
 
 
-function App(props) {
-  const videoData_nextVideos = videoData.map((item)=>{
-    console.log(item)
-    return (
-      <div className="App">
-        <Header />
-        <SelectedVideo />
-        <Comments />
-        <NextVideos
-        nextVideosImage = {item.image}
-        nextVideosTitle = {item.title}
-        nextVideosAuthor = {item.channel} />
-  
-      </div>
-    )
-  }
+function App() {
+
+  const {selectedVideos, setSelectedVideos} = useState(videoData[0]);
+
+  return (
+    <div className="App">
+      <Header />
+      <SelectedVideo />
+      <Comments />
+      <NextVideos
+        nextVideosImage = {videoData.map((item)=>{return item.image})}
+        nextVideosTitle = {videoData.map((item)=>{return item.title})}
+        nextVideosAuthor = {videoData.map((item)=>{return item.channel})} />
+    </div>
   )
 }
+
 
 export default App;

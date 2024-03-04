ceac75d52469a9431960bb94c5529aace6767a77
DW <danwang809@gmail.com>
2024-03-03 21:16:35 -0800

    update tablet and desktop for upload page

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -12,39 +12,45 @@ function UploadPage (){
         alert("Upload successful!");
         navigate("/");
     }
+
     function handleCancelButton (){
         alert("Cancelled uploading and going back to home page");
         navigate("/");
     }
 
     return (
-        <div className="upload">
+        <>
             <h2 className="upload__title">Upload Video</h2>
-            <p className="upload__thumbnail">VIDEO THUMBNAIL</p>
-            <img className="upload__image" src={image} alt="" />
-            <form onSubmit={handleFormSubmit}>
-                <p className="upload__yourVideo">TITLE YOUR VIDEO</p>
-                <input className="upload__titleInput" type="text" placeholder="Add a title to your video" />
-                <p className="upload__yourVideo">ADD A VIDEO DESCRIPTION </p>
-                <textarea className="upload__descriptionInput" placeholder="Add a description to your video"></textarea>
-                <div className="upload__button-group">
-                    <button className="upload__button-left" type="submit">
-                        <img className="upload__Icon" src={UploadIcon} alt="upload-Icon" />
-                        PUPLISH
-                    </button>
+            <div className="upload">
+                <section className="upload__videoThumbnail">
+                    <h2 className="upload__thumbnail">VIDEO THUMBNAIL</h2>
+                    <img className="upload__image" src={image} alt="" />
+                </section>
 
-                    <button className="upload__cancel" type="cancel" onclick={handleCancelButton}>
-                        CANCEL
-                    </button>
+                <form onSubmit={handleFormSubmit} className="upload__form">
+                    <p className="upload__yourVideo">TITLE YOUR VIDEO</p>
+                    <input className="upload__titleInput" type="text" placeholder="Add a title to your video" />
+                    <p className="upload__yourVideo">ADD A VIDEO DESCRIPTION </p>
+                    <textarea className="upload__descriptionInput" placeholder="Add a description to your video"></textarea>
+                    <div className="upload__button-group">
+                        <button className="upload__button-left" type="submit">
+                            <img className="upload__Icon" src={UploadIcon} alt="upload-Icon" />
+                            PUPLISH
+                        </button>
 
-                    <button className="upload__button-right" type="submit">
-                        <img className="upload__Icon" src={UploadIcon} alt="upload-Icon" />
-                        PUPLISH
-                    </button>
-                </div>
+                        <button className="upload__cancel" type="cancel" onclick={handleCancelButton}>
+                            CANCEL
+                        </button>
 
-            </form>
-        </div>
+                        <button className="upload__button-right" type="submit">
+                            <img className="upload__Icon" src={UploadIcon} alt="upload-Icon" />
+                            PUPLISH
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+        </>
     )
 }
 export default UploadPage;
